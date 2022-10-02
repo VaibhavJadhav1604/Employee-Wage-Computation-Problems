907e9c3adfc4bf46984e6cbfc151c28063388874
Vaibhav Jadhav <112764139+VaibhavJadhav1604@users.noreply.github.com>
2022-10-02 19:44:25 +0530

    Calculate Daily Employee Wage Using Switch Case
    Program To Calculate Daily Employee Wage Using Switch Case

---
+++ UC4.cs
@@ -0,0 +1,26 @@
+﻿using System.Linq.Expressions;
+
+const int isft = 1;
+const int ispt = 0;
+int emphr = 0;
+const int emprateperhr = 20;
+int empwage = 0;
+
+Random ran = new Random();
+int empcheck = ran.Next(0, 3);
+
+switch (empcheck)
+{
+    case isft:
+        emphr = 8;
+        break;
+    case ispt:
+        emphr = 4;
+        break;
+    default:
+        emphr = 0;
+        break;
+}
+
+empwage = emphr * emprateperhr;
+Console.WriteLine("Total Wage Is:"+empwage);
