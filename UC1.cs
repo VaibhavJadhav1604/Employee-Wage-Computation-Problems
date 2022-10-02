737a4c527bcd809aae1f4d3434cd0b2a4108955d
Vaibhav Jadhav <112764139+VaibhavJadhav1604@users.noreply.github.com>
2022-10-02 19:23:42 +0530

    Check Employee Is Present Or Absent
    Program To Check Employee Is Present Or Absent

---
+++ UC1.cs
@@ -0,0 +1,11 @@
+﻿int isft = 1;
+Random ran = new Random();
+int empcheck = ran.Next(0, 2);
+if (empcheck == isft)
+{
+    Console.WriteLine("Employee Is Present");
+}
+else
+{
+    Console.WriteLine("Employee Is Absent");
+}
