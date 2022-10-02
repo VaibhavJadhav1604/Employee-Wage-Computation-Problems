d44bd1b9feedca24529b6f98b7c5a2b2cabd1295
Vaibhav Jadhav <112764139+VaibhavJadhav1604@users.noreply.github.com>
2022-10-02 19:28:43 +0530

    Calculate Employee Wage Based On Time
    Program To Calculate Employee Wage Full Time Or Part Time

---
+++ UC3.cs
@@ -0,0 +1,24 @@
+﻿int isft = 1;
+int ispt = 0;
+int emphr = 0;
+int emprateperhr = 20;
+int empwage = 0;
+
+Random ran=new Random();
+int empcheck=ran.Next(0,3);
+
+if(empcheck==isft)
+{
+    emphr= 8;
+}
+else if(empcheck==ispt)
+{
+    emphr = 4;
+}
+else
+{
+    emphr = 0;
+}
+
+empwage = emphr * emprateperhr;
+Console.WriteLine("Total Employee Wage Is:"+ empwage);
